47ad776f127cd9105230dbf00c2d17edf80b64b9
Charlie Wang <chwa@google.com>
2023-08-31 13:59:02 +0000

    add automatic gcloud auth

--- scripts/session-resume.sh
+++ scripts/session-resume.sh
@@ -33,6 +33,5 @@ section_open "Setting Google Application Default Credentials"
     set_application_default_credentials "${SOURCE_ROOT}"
 section_close
 
-# section_open  "Authenticate with additional OAuth 2.0 scopes needed to use the Google Analytics Admin API"
-#     gcloud auth application-default login --quiet --scopes="openid,https://www.googleapis.com/auth/userinfo.email,https://www.googleapis.com/auth/cloud-platform,https://www.googleapis.com/auth/sqlservice.login,https://www.googleapis.com/auth/analytics,https://www.googleapis.com/auth/analytics.edit,https://www.googleapis.com/auth/analytics.provision,https://www.googleapis.com/auth/analytics.readonly,https://www.googleapis.com/auth/accounts.reauth"
-# section_close
+set +o nounset
+set +o errexit
