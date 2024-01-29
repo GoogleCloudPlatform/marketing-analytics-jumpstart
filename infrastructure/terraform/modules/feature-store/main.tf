3bc42aa527088ab451b843939ceaa0ee017aeba6
Charlie Wang <2144018+kingman@users.noreply.github.com>
2024-01-29 17:58:15 +0100

    update installation instructions (#74)
    * update installation instructions
    
    * typo
    
    * initialize log tables
    
    * initialize aggregated predictions table

--- infrastructure/terraform/modules/feature-store/main.tf
+++ infrastructure/terraform/modules/feature-store/main.tf
@@ -17,18 +17,18 @@ data "local_file" "config_vars" {
 }
 
 locals {
-  config_vars                        = yamldecode(data.local_file.config_vars.content)
-  config_bigquery                    = local.config_vars.bigquery
-  feature_store_project_id           = local.config_vars.bigquery.dataset.feature_store.project_id
-  purchase_propensity_project_id     = local.config_vars.bigquery.dataset.purchase_propensity.project_id
-  audience_segmentation_project_id   = local.config_vars.bigquery.dataset.audience_segmentation.project_id
-  auto_audience_segmentation_project_id   = local.config_vars.bigquery.dataset.auto_audience_segmentation.project_id
-  customer_lifetime_value_project_id = local.config_vars.bigquery.dataset.customer_lifetime_value.project_id
-  project_id                         = local.feature_store_project_id
-  sql_dir                            = var.sql_dir_input
-  builder_repository_id              = "marketing-data-engine-base-repo"
-  cloud_build_service_account_name   = "cloud-builder-runner"
-  cloud_build_service_account_email  = "${local.cloud_build_service_account_name}@${local.project_id}.iam.gserviceaccount.com"
+  config_vars                           = yamldecode(data.local_file.config_vars.content)
+  config_bigquery                       = local.config_vars.bigquery
+  feature_store_project_id              = local.config_vars.bigquery.dataset.feature_store.project_id
+  purchase_propensity_project_id        = local.config_vars.bigquery.dataset.purchase_propensity.project_id
+  audience_segmentation_project_id      = local.config_vars.bigquery.dataset.audience_segmentation.project_id
+  auto_audience_segmentation_project_id = local.config_vars.bigquery.dataset.auto_audience_segmentation.project_id
+  customer_lifetime_value_project_id    = local.config_vars.bigquery.dataset.customer_lifetime_value.project_id
+  project_id                            = local.feature_store_project_id
+  sql_dir                               = var.sql_dir_input
+  builder_repository_id                 = "marketing-data-engine-base-repo"
+  cloud_build_service_account_name      = "cloud-builder-runner"
+  cloud_build_service_account_email     = "${local.cloud_build_service_account_name}@${local.project_id}.iam.gserviceaccount.com"
 }
 
 module "project_services" {
