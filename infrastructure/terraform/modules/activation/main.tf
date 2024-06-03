326893faacf55b89b3812472b2c25c1f5b53c673
Carlos Timoteo <chmstimoteo@gmail.com>
2024-06-03 17:06:59 -0400

    Tf dependecies ds pipe (#139)
    * predicting for only the users with traffic in the past 72h - purchase propensity
    
    * running inference only for users events in the past 72h
    
    * including 72h users for all models predictions
    
    * considering null values in TabWorkflow models
    
    * deleting unused pipfile
    
    * upgrading lib versions
    
    * implementing reporting preprocessing as a new pipeline
    
    * adding more code documentation
    
    * adding important information on the main README.md and DEVELOPMENT.md
    
    * adding schedule run name and more code documentation
    
    * implementing a new scheduler using the vertex ai sdk & adding user_id to procedures for consistency
    
    * adding more code documentation
    
    * adding code doc to the python custom component
    
    * adding more code documentation
    
    * fixing aggregated predictions query
    
    * removing unnecessary resources from deployment
    
    * Writing MDS guide
    
    * adding the MDS developer and troubleshooting documentation
    
    * adding more info on the github access token creation
    
    * blurring image
    
    * seeting deployment dependency in dat store resources
    
    * fixing dependency for dataform-workflow moduke
    
    * fixing dependency flow for pipelines module
    
    * fixing waiting times for services apis
    
    ---------
    
    Co-authored-by: Carlos Timoteo <ctimoteo@google.com>

--- infrastructure/terraform/modules/activation/main.tf
+++ infrastructure/terraform/modules/activation/main.tf
@@ -100,7 +100,7 @@ resource "null_resource" "check_bigquery_api" {
     MAX_TRIES=100
     while ! gcloud services list --project=${module.project_services.project_id} | grep -i "bigquery.googleapis.com" && [ $COUNTER -lt $MAX_TRIES ]
     do
-      sleep 3
+      sleep 6
       printf "."
       COUNTER=$((COUNTER + 1))
     done
@@ -138,7 +138,7 @@ resource "null_resource" "check_artifactregistry_api" {
     MAX_TRIES=100
     while ! gcloud services list --project=${module.project_services.project_id} | grep -i "artifactregistry.googleapis.com" && [ $COUNTER -lt $MAX_TRIES ]
     do
-      sleep 3
+      sleep 6
       printf "."
       COUNTER=$((COUNTER + 1))
     done
@@ -164,7 +164,7 @@ resource "null_resource" "check_pubsub_api" {
     MAX_TRIES=100
     while ! gcloud services list --project=${module.project_services.project_id} | grep -i "pubsub.googleapis.com" && [ $COUNTER -lt $MAX_TRIES ]
     do
-      sleep 3
+      sleep 6
       printf "."
       COUNTER=$((COUNTER + 1))
     done
