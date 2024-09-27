30fc6d4dbee7a186f5b86a497e2652681a549be9
Carlos Timoteo <chmstimoteo@gmail.com>
2024-09-27 13:08:09 -0400

    Update quick-install.sh

--- scripts/quick-install.sh
+++ scripts/quick-install.sh
@@ -38,6 +38,10 @@ section_open  "Setting the gcloud project id"
     gcloud config set project "${TF_STATE_PROJECT_ID}"
 section_close
 
+section_open "Enable all the required APIs"
+    enable_all_apis
+section_close
+
 section_open "Authenticate to Google Cloud Project"
     gcloud auth login --project "${TF_STATE_PROJECT_ID}"
     echo "Close the browser tab that was open and press any key to continue.."
@@ -53,6 +57,19 @@ section_open "Setting Google Application Default Credentials"
     export GOOGLE_APPLICATION_CREDENTIALS=${CREDENTIAL_FILE}
 section_close
 
+section_open "Check OS system"
+    unameOut="$(uname -s)"
+    case "${unameOut}" in
+        Linux*)     machine=Linux;;
+        Darwin*)    machine=Mac;;
+        CYGWIN*)    machine=Cygwin;;
+        MINGW*)     machine=MinGw;;
+        MSYS_NT*)   machine=Git;;
+        *)          machine="UNKNOWN:${unameOut}"
+    esac
+    echo ${machine}
+section_close
+
 section_open "Configuring environment"
     SOURCE_ROOT=$(pwd)
     cd ${SOURCE_ROOT}
@@ -60,19 +77,19 @@ section_open "Configuring environment"
     # Install python3.10
     sudo chown -R ctimoteo /usr/local/sbin
     chmod u+w /usr/local/sbin
-    brew install python@3.10
-    retVal=$?
-    if [ $retVal -ne 0 ]; then
-        apt-get install python3.10
+    if [ $machine == "Linux" ]; then
+        sudo DEBIAN_FRONTEND=noninteractive apt-get -qq -o=Dpkg::Use-Pty=0 install python3.10 --assume-yes
+    elif [ $machine == "Darwin" ]; then
+        brew install python@3.10
     fi
     CLOUDSDK_PYTHON=python3.10
 
     # Install pipx
-    brew install pipx
-    retVal=$?
-    if [ $retVal -ne 0 ]; then
+    if [ $machine == "Linux" ]; then
         sudo apt update
         sudo apt install pipx
+    elif [ $machine == "Darwin" ]; then
+        brew install pipx
     fi
     pipx ensurepath
     
@@ -83,19 +100,12 @@ section_open "Configuring environment"
     poetry --version
 
     # Install tfenv
-    tfenv --version
-    retVal=$?
-    if [ $retVal -ne 0 ]; then
+    if [ ! -d ~/.tfenv ]; then
         git clone --depth=1 https://github.com/tfutils/tfenv.git ~/.tfenv
         echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
         echo 'export PATH=$PATH:$HOME/.tfenv/bin' >> ~/.bashrc
     fi
     export PATH="$PATH:$HOME/.tfenv/bin"
-
-    #mkdir -p ~/.local/bin/
-    #. ~/.profile
-    #ln -s ~/.tfenv/bin/* ~/.local/bin
-    #which tfenv
 
     # Install terraform version
     tfenv install 1.5.7
@@ -108,12 +118,16 @@ section_close
 
 section_open "Preparing Terraform Environment File"
     TERRAFORM_RUN_DIR=${SOURCE_ROOT}/infrastructure/terraform
-    cp -v $TERRAFORM_RUN_DIR/terraform-sample.tfvars $TERRAFORM_RUN_DIR/terraform.tfvars
-    echo "Edit the terraform.tfvars created and press any key to continue.."
-    read moveon
+    if [ ! -f $TERRAFORM_RUN_DIR/terraform.tfvars ]; then
+        . scripts/set-env.sh
+        sudo apt-get -qq -o=Dpkg::Use-Pty=0 install gettext
+        envsubst < "${SOURCE_ROOT}/infrastructure/cloudshell/terraform-template.tfvars" > "${TERRAFORM_RUN_DIR}/terraform.tfvars"
+    fi
 section_close
 
 section_open "Deploying Terraform Infrastructure Resources"
+    export PATH="$HOME/.local/bin:$PATH"
+    export PATH="$PATH:$HOME/.tfenv/bin"
     terraform -chdir="${TERRAFORM_RUN_DIR}" init
     terraform -chdir="${TERRAFORM_RUN_DIR}" apply
 section_close
